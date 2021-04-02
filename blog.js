a8fe127dc77510513ae11acd6dc46221546e39d5
nimastudent <74091318+nimastudent@users.noreply.github.com>
2021-04-02 12:44:53 +0800

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,17 @@
+const blog = [
+    {
+        id:1,
+        title:"这是我的第一篇博客",
+        Image:"./image/1.jpg"
+    },
+    {
+        id:2,
+        title:"这是我的第二篇博客",
+        Image:"./image/2.jpg"
+    },
+    {
+        id:3,
+        title:"这是我的第三篇博客",
+        Image:"./image/3.jpg"
+    },
+]
